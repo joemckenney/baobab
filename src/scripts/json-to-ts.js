c1fad9f1f4926145a6de3c0abd8bc6d47e67e753
joemckenney <j.mckenney00@gmail.com>
2021-02-23 08:57:27 -0800

    Checkpoint

--- src/scripts/json-to-ts.js
+++ src/scripts/json-to-ts.js
@@ -6,19 +6,55 @@ import { readdirSync, readFileSync, statSync, writeFileSync } from 'fs'
 import { join, resolve } from 'path'
 const themes = resolve('./src/themes/')
 
+import {
+  quicktype,
+  InputData,
+  jsonInputForTargetLanguage,
+} from 'quicktype-core'
+
+async function quicktypeJSON(targetLanguage, typeName, jsonString) {
+  const jsonInput = jsonInputForTargetLanguage(targetLanguage)
+
+  // We could add multiple samples for the same desired
+  // type, or many sources for other types. Here we're
+  // just making one type from one piece of sample JSON.
+  await jsonInput.addSource({
+    name: typeName,
+    samples: [jsonString],
+  })
+
+  const inputData = new InputData()
+  inputData.addInput(jsonInput)
+
+  return await quicktype({
+    inputData,
+    inferMaps: false,
+    rendererOptions: {
+      'just-types': true,
+    },
+    lang: targetLanguage,
+  })
+}
+
 readdirSync(themes)
   .filter((f) => statSync(join(themes, f)).isDirectory())
-  .forEach((themeName) => {
-    const colors = JSON.parse(
-      readFileSync(resolve(`./src/themes/${themeName}/colors.json`)).toString()
-    )
+  .forEach(async (themeName) => {
+    const colorString = readFileSync(
+      resolve(`./src/themes/${themeName}/colors.json`)
+    ).toString()
+    const colors = JSON.parse(colorString)
+
+    const { lines } = await quicktypeJSON('typescript', 'Colors', colorString)
+
     prettier.resolveConfig(process.cwd()).then((options) => {
       writeFileSync(
         `./src/themes/${themeName}/colors.ts`,
         prettier.format(
           `export type Color = ${Object.keys(dotNotation(colors))
             .map((key) => `| '${key}'`)
-            .join('')}`,
+            .join('')}
+            ${lines.join('\n')}
+          `,
           { parser: 'typescript', ...options }
         )
       )
--- src/themes/albany-bowl/colors.ts
+++ src/themes/albany-bowl/colors.ts
@@ -19,3 +19,49 @@ export type Color =
   | 'layout.blacks.300'
   | 'layout.whites.100'
   | 'layout.whites.200'
+export interface Colors {
+  brand: Brand
+  semantic: Semantic
+  layout: Layout
+}
+
+export interface Brand {
+  'dark-liver': string
+  'cyber-grape': string
+  'cadet-blue': string
+  aquamarine: string
+  'bitter-lime': string
+}
+
+export interface Layout {
+  grays: Grays
+  blacks: Blacks
+  whites: Whites
+}
+
+export interface Blacks {
+  '100': string
+  '200': string
+  '300': string
+}
+
+export interface Grays {
+  '100': string
+  '200': string
+  '300': string
+  '400': string
+  '500': string
+  '600': string
+  '700': string
+}
+
+export interface Whites {
+  '100': string
+  '200': string
+}
+
+export interface Semantic {
+  success: string
+  error: string
+  warning: string
+}
